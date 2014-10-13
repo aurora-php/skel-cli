abb3fa5d73d9cb4d46e76131fe7e101b2a3cb7ea
Harald Lapp <harald@octris.org>
2014-10-13 03:46:30 +0200

    allow to use configuration files from cli app

--- data/skel/cli/phar/stub.php
+++ data/skel/cli/phar/stub.php
@@ -34,6 +34,15 @@ Phar::mapPhar();
 
 require_once('phar://{{$module}}.phar/libs/autoloader.class.php');
 
+// load application configuration
+$registry = \org\octris\core\registry::getInstance();
+$registry->set('OCTRIS_APP', '{{$directory}}', \org\octris\core\registry::T_READONLY);
+$registry->set('OCTRIS_BASE', __DIR__, \org\octris\core\registry::T_READONLY);
+$registry->set('config', function() {
+    return new \org\octris\core\config('{{$directory}}', 'config');
+}, \org\octris\core\registry::T_SHARED | \org\octris\core\registry::T_READONLY);
+
+// run application
 $main = new {{$namespace}}\main();
 $main->run();
 
