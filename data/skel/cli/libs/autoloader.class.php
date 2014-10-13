823b2bf0dfd73e9f1d0f87a3b219a885f5c8810c
Harald Lapp <harald@octris.org>
2014-10-13 02:34:42 +0200

    moved autoloader to own class

---
+++ data/skel/cli/libs/autoloader.class.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of the '{{$directory}}' package.
+ *
+ * (c) {{$company}}
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace {{$module}} {
+    /**
+     * Autoloader.
+     *
+     * @octdoc      c:libs/main
+     * @copyright   copyright (c) {{$year}} by {{$company}}
+     * @author      {{$author}} <{{$email}}>
+     */
+    class autoloader
+    /**/
+    {
+        /**
+         * Class Autoloader.
+         *
+         * @octdoc  m:autoloader/autoload
+         * @param   string          $classpath              Path of class to load.
+         */
+        public static function autoload($classpath)
+        /**/
+        {
+            if (strpos($classpath, '{{$module}}\\') === 0) {
+                $file = __DIR__ . '/' . str_replace('\\', '/', substr($classpath, strlen('{{$module}}'))) . '.class.php';
+            } else {
+                $classpath = preg_replace('|\\\\|', '.', ltrim($classpath, '\\'), 2);
+                $classpath = preg_replace('|\\\\|', '/libs/', $classpath, 1);
+                $classpath = str_replace('\\', '/', $classpath);
+                
+                $file = __DIR__ . '/../vendor/' . $classpath . '.class.php';
+            }
+            
+            if (file_exists($file)) {
+                require_once($file);
+            }
+        }
+    }
+    
+    spl_autoload_register(array('\{{$module}}\autoloader', 'autoload'));
+}
