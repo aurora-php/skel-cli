8891600197198f946d9530422f948c971a90898a
Harald Lapp <harald@octris.org>
2014-10-26 15:09:08 +0100

    fixed namespaces

--- data/skel/cli/libs/app.php
+++ data/skel/cli/libs/app.php
@@ -9,61 +9,61 @@
  * file that was distributed with this source code.
  */
 
-namespace {{$namespace}} {
-    /**
-     * Application class.
-     *
-     * @octdoc      c:libs/app
-     * @copyright   copyright (c) {{$year}} by {{$company}}
-     * @author      {{$author}} <{{$email}}>
-     */
-    class app extends \octris\cliff\app
-    {
-        /**
-         * Application name.
-         *
-         * @octdoc  p:app/$app_name
-         * @type    string
-         */
-        protected static $app_name = '{{$vendor}}-{{$module}}';
-        /**/
-        
-        /**
-         * Application version.
-         *
-         * @octdoc  p:app/$app_version
-         * @type    string
-         */
-        protected static $app_version = '0.0.0';
-        /**/
-        
-        /**
-         * Application version date.
-         *
-         * @octdoc  p:app/$app_version_date
-         * @type    string
-         */
-        protected static $app_version_date = '0000-00-00';
-        /**/
-        
-        /**
-         * Constructor.
-         *
-         * @octdoc  m:app/__construct
-         */
-        public function __construct()
-        {
-        }
+namespace {{$namespace}};
 
-        /**
-         * Execute application.
-         *
-         * @octdoc  m:app/main
-         * @param   \octris\cliff\args\collection        $args           Parsed arguments.
-         */
-        protected function main(\octris\cliff\args\collection $args);
-        {
-        }
+/**
+ * Application class.
+ *
+ * @octdoc      c:libs/app
+ * @copyright   copyright (c) {{$year}} by {{$company}}
+ * @author      {{$author}} <{{$email}}>
+ */
+class app extends \octris\cliff\app
+{
+    /**
+     * Application name.
+     *
+     * @octdoc  p:app/$app_name
+     * @type    string
+     */
+    protected static $app_name = '{{$vendor}}-{{$module}}';
+    /**/
+    
+    /**
+     * Application version.
+     *
+     * @octdoc  p:app/$app_version
+     * @type    string
+     */
+    protected static $app_version = '0.0.0';
+    /**/
+    
+    /**
+     * Application version date.
+     *
+     * @octdoc  p:app/$app_version_date
+     * @type    string
+     */
+    protected static $app_version_date = '0000-00-00';
+    /**/
+    
+    /**
+     * Constructor.
+     *
+     * @octdoc  m:app/__construct
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Execute application.
+     *
+     * @octdoc  m:app/main
+     * @param   \octris\cliff\args\collection        $args           Parsed arguments.
+     */
+    protected function main(\octris\cliff\args\collection $args);
+    {
     }
 }
 
